fee07ad3b05c8f61303999fe4e717f106a8df51d
Zach Wellens <zachwellens990@gmail.com>
2023-06-05 15:57:08 -0400

    Added the questions

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "What text do you want in the logo? (3 letter maximum)",
+        // Is this right?
+        validate: (ans) => {
+            if (ans.length <= 3) {
+                return true;
+            } else {
+                return "Input is to large.";
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "What color do yuo want the text to be? \nInput the name (ie. 'green') or hexcode (ie. #1eff00)"
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "What shaape do you want to use?",
+        choices: ["circle", "triangle", "square"]
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "What color do yuo want the shape to be? \nInput the name (ie. 'green') or hexcode (ie. #1eff00)"
+    },
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.error(err) : console.log('README successfully created!')
+    );
+}
+
+inquirer.prompt(questions).then((response) => {
+    // TODO: write the file
+});
